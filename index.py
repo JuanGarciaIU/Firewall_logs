c13035e36591a6834f6d7c769b9e8aecf821c339
Juan Garcia <garcia70iu.edu>
2024-03-05 09:22:22 -0500

    Analyzer v3

--- index.py
+++ index.py
@@ -4,21 +4,26 @@ from log_analyzer import LogEntry
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Log Analyzer Program")
-    parser.add_argument("--filename", help="CSV file containing log data", required=True)
+    parser.add_argument("--filename","-f", help="CSV file containing log data", required=True)
+    parser.add_argument("--action","-a", help="Actions preformed", choices=["head"], required=True)
     return parser.parse_args()
 
+def print_head(log_entries):
+  for log_entry in log_entries[:5]:
+        print(log_entry.event_time.strftime("%m/%d/%Y %H:%M %Z"),",", log_entry.action,",", log_entry.source_ip, ",", log_entry.ipv4_class, ",", log_entry.country_name)
+
+
 def main():
+
     args = parse_arguments()
     filename = args.filename
 
     log_entries = []
 
-    # Read data from the CSV file
     with open(filename, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         for row in csv_reader:
-            # Create LogEntry objects from CSV data
             log_entry = LogEntry(
                 row['event_time'],
                 row['internal_ip'],
@@ -27,19 +32,13 @@ def main():
                 row['action'],
                 row['rule_id'],
                 row['source_ip'],
-                country=row.get('country'),  # Providing default values for optional arguments
+                country=row.get('country'), 
                 country_name=row.get('country_name')
             )
             log_entries.append(log_entry)
 
-    # Print information for the first five LogEntry objects
-    for log_entry in log_entries[:5]:
-        print("Date:", log_entry.event_time.strftime("%m/%d/%Y %H:%M %Z"), "in Eastern Time")
-        print("Action:", log_entry.action)
-        print("Source IP:", log_entry.source_ip)
-        print("IPv4 Class:", log_entry.ipv4_class)
-        print("Country Name:", log_entry.country_name)
-        print()
+    if action == "head":
+        print_head(log_entries)
 
 if __name__ == "__main__":
     main()
